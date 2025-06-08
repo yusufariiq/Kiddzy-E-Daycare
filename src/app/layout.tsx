cf6b55c71708b8c2db6f526c01cac06237295095
Ariiq Yusuf <yusufariiq3@gmail.com>
2025-06-08 17:55:24 +0700

    fix  layout for admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -5,32 +7,38 @@ import Navbar from "@/components/common/navbar";
 import Footer from "@/components/common/footer";
 import { AuthProvider } from "@/context/auth.context";
 import { Toaster } from "react-hot-toast";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({
   weight: ['400', '900'],
   subsets: ['latin'],
 })
 
-export const metadata: Metadata = {
-  title: "Kiddzy E-Daycare",
-  description: "A platform designed for parents to discover and book childcare services.",
-};
+// export const metadata: Metadata = {
+//   title: "Kiddzy E-Daycare",
+//   description: "A platform designed for parents to discover and book childcare services.",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const pathname = usePathname()
+  const hideLayout = pathname.startsWith("/admin")
+  const hideFooter = pathname.startsWith("/auth")
+
   return (
     <html lang="en">
-      <body
+      <body 
         className={`${inter} ${inter} antialiased`}
       >
         <AuthProvider>
-          <Navbar/>
+          {!hideLayout && <Navbar />}
           {children}
           <Toaster position="top-right" reverseOrder={false} />
-          <Footer/>
+          {!hideLayout && !hideFooter && <Footer />}
         </AuthProvider>
       </body>
     </html>
