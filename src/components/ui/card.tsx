7d53117228fe507899f636b6b2e09cf8b6617af6
Ariiq Yusuf <yusufariiq3@gmail.com>
2025-06-10 22:05:12 +0700

    fix card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/utils/utils"
 import * as React from "react"
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props} />
+  <div ref={ref} className={cn("rounded-lg bg-white text-card-foreground", className)} {...props} />
 ))
 Card.displayName = "Card"
 
